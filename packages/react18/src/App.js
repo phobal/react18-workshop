1bf7c9c40a7edd21f327c12b193ea5f5ff10d16a
phobal <phobal@126.com>
2022-04-19 15:02:09 +0800

    feat: setState batchedUpdates

--- packages/react18/src/App.js
+++ packages/react18/src/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0)
+  const [flag, setFlag] = useState(false)
+  const onClick = () => {
+    Promise.resolve().then(() => {
+      setCount(count++)
+    })
+    setFlag(!flag)
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={onClick}>测试 rerender</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,6 +26,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React 18
+          {console.log('----rerender----')}
         </a>
       </header>
     </div>
